bd4f4f3b9c5377fc76e4961c1eb239ad30080294
Rafael Silva e Souza <rafael.souza@arctouch.com>
2020-09-05 10:11:23 -0300

    Project created

---
+++ lib/cubit_app.dart
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 ArcTouch LLC.
+ * All rights reserved.
+ *
+ * This file, its contents, concepts, methods, behavior, and operation
+ * (collectively the "Software") are protected by trade secret, patent,
+ * and copyright laws. The use of the Software is governed by a license
+ * agreement. Disclosure of the Software to third parties, in any form,
+ * in whole or in part, is expressly prohibited except as authorized by
+ * the license agreement.
+ */
+
+import 'package:flutter/material.dart';
+
+class CubitApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cubit Study',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Cubits!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 ArcTouch LLC.
+ * All rights reserved.
+ *
+ * This file, its contents, concepts, methods, behavior, and operation
+ * (collectively the "Software") are protected by trade secret, patent,
+ * and copyright laws. The use of the Software is governed by a license
+ * agreement. Disclosure of the Software to third parties, in any form,
+ * in whole or in part, is expressly prohibited except as authorized by
+ * the license agreement.
+ */
+
+import 'package:cubit_study/cubit_app.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+
+void main() {
+  EquatableConfig.stringify = true;
+
+  // Initialize simple logging
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(
+    (l) => print('${l.time} - ${l.loggerName} - ${l.level.name}: ${l.message}'),
+  );
+
+  runApp(
+    CubitApp(),
+  );
+}
